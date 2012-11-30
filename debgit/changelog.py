cb3d33a7620102093689284d716fc59bc5de11ea
Roberto Aguilar <roberto@baremetal.io>
2012-11-29 17:37:43 -0800

    Refactored debgit-get-changelog-entries
    Pulled out all changelog logic and added it to the debgit package.

---
+++ debgit/changelog.py
@@ -0,0 +1,29 @@
+"""
+Module for managing changelog information
+"""
+import os
+
+import sh
+
+from pygit2 import Repository
+
+def get_changelog_entries():
+    """
+    Iterate through any changelog entries found
+    """
+    last_tag = None
+
+    try:
+        last_tag = str(sh.git('describe', '--tags', '--abbrev=0', 'HEAD^').strip())
+        ref = '{0}..{1}'.format(last_tag, 'HEAD')
+    except sh.ErrorReturnCode_128:
+        ref = 'HEAD'
+
+    repo = Repository('.git')
+
+    for line in sh.git('rev-list', ref, _iter=True):
+        commit_hash = line.strip()
+        commit = repo[commit_hash]
+
+        if 'Changelog:' in commit.message:
+            yield commit.message.splitlines()[0]
