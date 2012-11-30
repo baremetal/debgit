73f10a31fc0057a644b606dd8883d184d5f325be
Roberto Aguilar <roberto@baremetal.io>
2012-11-29 17:37:43 -0800

    Refactored debgit-get-version
    Pulled out all versioning logic and added it to the debgit package.

---
+++ debgit/version.py
@@ -0,0 +1,32 @@
+"""
+Module for managing git versions.
+"""
+import time
+
+import sh
+
+def get_version():
+    git_version = ''
+    git_sha = None
+    now = time.strftime('%y%j.%H%M')
+
+    try:
+        git_describe = str(sh.git('describe', tags=True).strip())
+        git_describe_split = git_describe.split('-')
+
+        git_version = git_describe_split[0]
+
+        if len(git_describe_split) > 1:
+            git_sha = git_describe_split[-1]
+    except sh.ErrorReturnCode_128:
+        git_version = '0.0'
+
+    # when no sha is found, we are on an actual tag rather than development
+    # from a particular tag.  in this case, simply use the bare version
+    # number rather than generating a date+sha-based version number.
+    if git_sha:
+        sw_version = '{0}+{1}+{2}'.format(git_version, now, git_sha)
+    else:
+        sw_version = git_version
+
+    return sw_version
