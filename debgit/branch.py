2bb13abfeed309a958faadc9e03660315249f739
Roberto Aguilar <roberto@baremetal.io>
2012-11-30 01:57:19 -0800

    Added get_branch_list() to reprepro.branch

--- debgit/branch.py
+++ debgit/branch.py
@@ -5,3 +5,11 @@ import sh
 
 def get_branch_name():
     return str(sh.basename(str(sh.git('symbolic-ref', 'HEAD')))).strip()
+
+def get_branch_list():
+    """
+    return a list of all the branches in a given repository
+    """
+
+    for line in sh.git('branch', '-a', _iter=True):
+        yield line[2:]
